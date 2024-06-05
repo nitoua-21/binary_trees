dafb5adb96345712f55d636061659c5cf84a7788
Normich ITOUA <nitoua.dev@gmail.com>
2024-06-06 00:05:42 +0100

    Add task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_sibling - Finds the sibling of a node in a binary tree
+* @node: Pointer to the node to find the sibling of
+*
+* Return: Pointer to the sibling node, or NULL if no sibling or if node is NULL
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL ||
+		node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
+
+/**
+* binary_tree_uncle - Finds the uncle of a node in a binary tree
+* @node: Pointer to the node to find the uncle of
+*
+* Return: Pointer to the uncle node, or NULL if no uncle or if node is NULL
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
