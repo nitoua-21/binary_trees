f3dbbd9164d37e3453d88e00ed4c7020e7300e7d
Normich ITOUA <nitoua.dev@gmail.com>
2024-06-05 23:34:04 +0100

    Fix task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,10 +14,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_hight = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	left_hight = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return (left_hight > right_height ? left_hight : right_height);
+	return (1 + (left_hight > right_height ? left_hight : right_height));
 }
 
 
